c8a4fcc79a9dad1bff4daa3af6df9f36a8f9b9aa
J Karkar <jkarkar@gmail.com>
2015-07-20 18:57:02 +0100

    BanksTest implemented for underlying Banks Class

--- bscg/src/bscg/Banks.java
+++ bscg/src/bscg/Banks.java
@@ -1,5 +1,27 @@
 package bscg;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Banks {
+
+	private List<Card> cards;  // List Type Interface
+
+	
+
+	//@formatter:off
+	public Banks() {cards = new ArrayList<Card>();}
+	//@formatter:on
+
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+	
+	public List<Card> getCards() {
+		return cards;
+	}
+	
+	
 
 }
---
+++ bscg/test/bscg/BanksTest.java
@@ -0,0 +1,36 @@
+package bscg;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BanksTest {
+
+	Banks banks;
+	Card card;
+	
+	@Before
+	public void setup() {
+		banks = new Banks();
+		card = new Card();
+	}
+	
+	
+	@Test
+	public void test_addCard() {
+		banks = new Banks();
+		card.createNewCard("HSBC Canada", "5601-2345-3446-5678", "Nov-2017");
+		banks.addCard(card);
+		List<Card> cards = (ArrayList<Card>) banks.getCards();
+		assertTrue(cards.contains(card));
+		for (Card card : cards) {
+			System.out.println(card.toString());
+		}
+		
+	}
+
+}
