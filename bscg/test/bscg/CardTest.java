fa3d50d9da49e8242da263e51b31ba60eb76a644
J Karkar <jkarkar@gmail.com>
2015-07-20 22:42:34 +0100

    CardTest uncommitted

--- bscg/test/bscg/CardTest.java
+++ bscg/test/bscg/CardTest.java
@@ -6,8 +6,6 @@ import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
 
-
-
 public class CardTest {
 
 	Card card = new Card();
@@ -29,26 +27,30 @@ public class CardTest {
 		FormatterUtil.addToStartWithList("56");
 		card.createNewCard("HSBC Canada", "5601-2345-3446-5678", "Nov-2017");
 		// assertEquals(bank, card.getBank());
-//		assertEquals("56xx-xxxx-xxxx-xxxx", card.getCardNumber());
-//		assertEquals(expiryDate, card.getExpiryDate());
+		// assertEquals("56xx-xxxx-xxxx-xxxx", card.getCardNumber());
+		// assertEquals(expiryDate, card.getExpiryDate());
 	}
 
 	@Test
 	public void test_toString() {
-	//	assertEquals(card.getCardNumber() + " " + "56xx-xxxx-xxxx-xxxx" + " " + card.getExpiryDate(),card.toString());
+		// assertEquals(card.getCardNumber() + " " + "56xx-xxxx-xxxx-xxxx" + " "
+		// + card.getExpiryDate(),card.toString());
 	}
-	
+
 	@Test
 	public void test_compareTo() {
 		Card oCard = new Card();
 		card.createNewCard("HSBC Canada", "5601-2345-3446-5678", "Nov-2017");
 		oCard.createNewCard("HSBC Canada", "5601-2345-3446-5678", "Nov-2017");
 		System.out.println(card.compareTo(oCard));
-		assertEquals("return 0 if they are equal - test key is expiry date",0, card.compareTo(oCard));
+		assertEquals("return 0 if they are equal - test key is expiry date", 0,
+				card.compareTo(oCard));
 		card.createNewCard("HSBC Canada", "5601-2345-3446-5678", "Nov-2017");
 		oCard.createNewCard("HSBC Canada", "5601-2345-3446-5678", "Nov-2018");
-		assertEquals("return -1 if they are not equal - test key is expiry date",-1, card.compareTo(oCard));
-		
+		assertEquals(
+				"return -1 if they are not equal - test key is expiry date",
+				-1, card.compareTo(oCard));
+
 		// this.expiryDate.compareTo(o.expiryDate); testing on expiryDate
 	}
 }
